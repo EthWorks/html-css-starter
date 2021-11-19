265c086775d13c7b29a9af5755647530d290b75c
Vladyslav Mazurenko <vladyslav@TPL005-VM-MBP17.localdomain>
2021-11-19 15:24:03 +0100

    Sass linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, watch, parallel, series } = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+const sassLint = require('gulp-sass-lint');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify-es').default;
@@ -34,9 +35,24 @@ function scripts() {
     .pipe(browserSync.stream())
 }
 
+function stylesLinter() {
+  return src('src/sass/**/*.sass')
+    .pipe(sassLint({
+      files: { ignore: 'src/sass/helpers/_reset.sass' },
+      rules: {
+        'mixins-before-declarations': 0
+      }
+    }))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+}
+
 function styles() {
   return src('src/sass/main.sass')
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 version'],
@@ -52,16 +68,18 @@ function build() {
     'src/fonts/**/*',
     'src/js/main.min.js',
     'src/**/*.html ',
-  ], {base: 'src'})
+  ], { base: 'src' })
     .pipe(dest('dist'))
 }
 
 function watching() {
-  watch(['src/sass/**/*.sass'], styles)
+  watch(['src/sass/**/*.sass'], stylesLinter).on('change', browserSync.reload)
+  watch(['src/sass/**/*.sass'], styles).on('change', browserSync.reload)
   watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts)
   watch(['src/**/*.html']).on('change', browserSync.reload)
 }
 
+exports.stylesLinter = stylesLinter;
 exports.styles = styles;
 exports.watching = watching;
 exports.browsersync = browsersync;
@@ -71,4 +89,3 @@ exports.clean = clean;
 
 exports.build = series(clean, images, build);
 exports.default = parallel(browsersync, watching)
- 
